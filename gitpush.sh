fb43b487f857bb8d1737a798d17a096ae453c645
Mteltn_Guernica <3111670200@qq.com>
2026-08-12 16:50:47 +0800

    C

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: Please provide a commit message."
+  echo "Usage: ./gpush.sh \"Your commit message\""
+  exit 1
+fi
+
+git add .
+git commit -m "$1"
+git push
+
+echo "Push complete!"
